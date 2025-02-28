1d836114ad8d2fdf9039b3a01a3e0bd8a2e64c74
Figary <yousseffigary@gmail.com>
2025-02-28 18:43:08 +0200

    the exeption package is done but the engine and the engine.board are not done yet

---
+++ Game/src/engine/board/CellType.java
@@ -0,0 +1,10 @@
+package engine.board;
+
+public enum CellType {
+	
+	NORMAL,
+	SAFE,
+	BASE,
+	ENTRY
+
+}
---
+++ Game/src/exception/ActionException.java
@@ -0,0 +1,12 @@
+package exception;
+
+abstract public class ActionException extends GameException {
+
+	public ActionException(){
+		super();
+	}
+	public ActionException(String message){
+		super(message);
+	}
+	
+}
