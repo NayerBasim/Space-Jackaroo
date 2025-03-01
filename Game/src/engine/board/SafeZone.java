819ccdf6de25413ae576cf497bb9ca5d71f4faa6
Figary <yousseffigary@gmail.com>
2025-03-01 03:03:37 +0200

    almost done the constractor of class game is still to be done

--- Game/src/engine/board/SafeZone.java
+++ Game/src/engine/board/SafeZone.java
@@ -1,13 +1,24 @@
 package engine.board;
 import java.util.ArrayList;
+import model.Colour;
+
 
 public class SafeZone {
 
-	final Colour colour;
-	final ArrayList<Cell> cells;
+	final private Colour colour; // read only
+	final private ArrayList<Cell> cells; // read only
 	
 	public SafeZone(Colour colour){
 		this.colour = colour;
-		// (to be done) creates 4 SAFE cells in the arraylist of cells.
+		for(int i =0;i<4;i++){
+			cells.add(Cell(CellType.SAFE)); // (to be debugged) 
+		}
+		
+	}
+	public Colour getColour(){
+		return colour;
+	}
+	public ArrayList<Cell> getCells(){
+		return cells;
 	}
 }
